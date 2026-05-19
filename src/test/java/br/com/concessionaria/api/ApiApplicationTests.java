ed7933dc815bc8a41a47446bd1b89f9b62bcb69d
Annajleite <annajuliadealmeidaleite12@gmail.com>
2026-05-18 23:30:46 -0300

    "Primeiro commit(estrutura spring boot)"

---
+++ src/test/java/br/com/concessionaria/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.concessionaria.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
